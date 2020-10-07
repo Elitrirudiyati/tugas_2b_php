4945294b0d09185004c684cc00f13408118ebd02
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-10-07 16:29:34 +0700

    Add files via upload

---
+++ tugas_2b.php
@@ -0,0 +1,19 @@
+<?php
+$jual=80000;
+$beli=50000;
+$untung=($beli/$jual)*100;
+
+echo "Jual =$jual";
+echo "<br>";
+echo "Beli =$beli";
+echo " <br>";
+echo "Keuntungan = ?";
+echo "<br>";
+echo "Jawaban : $untung%";
+
+
+
+
+?>
+
+
